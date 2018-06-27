8610ff5e3de6d95a2e91444404b76afdbd4a6b02
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-06-27 13:53:23 +0300

    output_statistic: if->dict, обработка ошибок

--- output_statistic.py
+++ output_statistic.py
@@ -1,18 +1,19 @@
 import csv
 import json
+import logging
 
 
 def output_statistic(statistic, output_type='stdout'):
-    if output_type == 'stdout':
-        output_to_stdout(statistic)
-    elif output_type == 'json':
-        output_to_json(statistic)
-    elif output_type == 'csv':
-        output_to_csv(statistic)
-    elif output_type == 'ods':
-        output_to_ods(statistic)
-    elif output_type == 'pdf':
-        output_to_pdf(statistic)
+    output_type_handlers = {
+        'stdout': output_to_stdout,
+        'json': output_to_json,
+        'csv': output_type,
+        'ods': output_to_ods,
+        'pdf': output_to_pdf,
+    }
+    output_handler = output_type_handlers.get(output_type)
+    if output_handler:
+        output_handler(statistic)
 
 
 def print_statistics_words_top(words_top, words_type='words'):
@@ -30,21 +31,30 @@ def print_statistics_words_top(words_top, words_type='words'):
 
 def output_to_json(statistic):
     statistics_json = json.dumps(statistic, sort_keys=True, indent=4)
-    with open('words_code_stat.json', 'w') as json_file:
-        json_file.write(statistics_json)
+    try:
+        with open('words_code_stat.json', 'w') as json_file:
+            json_file.write(statistics_json)
+    except IOError as e:
+        logging.error('words_code_stat.json I/O error({0}): {1}'.format(
+            e.errno, e.strerror
+        ))
 
 
 def output_to_csv(statistic):
-    with open('words_code_stat.csv', 'w') as csv_file:
-        statistic_writer = csv.writer(csv_file)
-        statistic_writer.writerow([statistic['word_type'], 'occurence'])
-        for word, occurence in statistic['words_top']:
-            statistic_writer.writerow([word, occurence])
+    try:
+        with open('words_code_stat.csv', 'w') as csv_file:
+            statistic_writer = csv.writer(csv_file)
+            statistic_writer.writerow([statistic['word_type'], 'occurence'])
+            for word, occurence in statistic['words_top']:
+                statistic_writer.writerow([word, occurence])
+    except IOError as e:
+        logging.error('words_code_stat.csv I/O error({0}): {1}'.format(
+            e.errno, e.strerror
+        ))
 
 
 def output_to_stdout(statistic):
     # Выводим на экран результаты
-    # print('total {0} files'.format(statistic['file_count']))
     print('statistic type: {0}'.format(statistic['parse_code_type']))
     print_statistics_words_top(statistic['words_top'],
                                words_type=statistic['word_type'])
